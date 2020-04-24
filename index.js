a32290853df1a5e6b85c8ae1559cbcf549b2a766
LoupeG94 <loupeidisgiannis@gmail.com>
2020-04-24 14:26:51 +0300

    modified css and js

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumButtons; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
     var buttonInnerHTML = this.innerHTML;
 
     makeSound(buttonInnerHTML);
-
-
+    buttonAnimation(buttonInnerHTML);
   });
-
 }
 
+
+
+document.addEventListener("keypress",function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
 
 
 function makeSound(key){
@@ -54,14 +57,14 @@ function makeSound(key){
       break;
 
     default: console.log(buttonInnerHTML);
-
   }
-
 }
 
 
-
-document.addEventListener("keypress",function(event){
-  makeSound(event.key);
-
-});
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
+}
